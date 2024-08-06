68eef4473cbda2a57e07679ae7ffe9cd4960f4f8
Parth Mehandru <130220083+Parthmeh27@users.noreply.github.com>
2024-08-06 21:53:40 +0530

    Add files via upload
    Added files to Repository

---
+++ assets/scripts/JavaScript/script1.js
@@ -0,0 +1,6 @@
+function check()
+    {
+        var a=parseFloat(window.document.my.FirstNumber.value);
+        var b=parseFloat(window.document.my.SecondNumber.value);
+        window.document.my.Sum.value=a+b;
+    }
